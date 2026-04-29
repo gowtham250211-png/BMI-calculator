6b733467fff9b90ab7da244127c9990199b39a09
Gowtham <gowtham250211@gmail.com>
2026-04-29 19:59:58 +0530

    first upload

---
+++ src/App.jsx
@@ -0,0 +1,80 @@
+import { useState } from 'react'
+import './App.css'
+
+function App(){
+  const[height,setHeight]=useState("");
+  const[weight,setWeight]=useState("");
+  const[bmi,setBmi]=useState(null);
+  const[bmiStatus,setBmiStatus]=useState("");
+  const[errorMessage,setErrorMessage]=useState("")
+
+  const calculateBmi = () =>{
+    const isValidHeight = /^\d+$/.test(height);
+    const isValidWeight = /^\d+$/.test(weight);
+    if(isValidHeight&&isValidWeight){
+      const heightInMeter = height / 100;
+      const bmiValue = weight / (heightInMeter * heightInMeter);
+      setBmi(bmiValue.toFixed(2));
+      if(bmiValue < 18.5) {
+        setBmiStatus("UnderWeight");
+      }
+      else if (bmiValue >= 18.5 && bmiValue < 24.9) {
+        setBmiStatus("NormalWeight");
+      }
+      else if (bmiValue >= 25 && bmiValue < 29.9) {
+        setBmiStatus("OverWeight");
+      }
+      else {
+        setBmiStatus("Obese");
+      }
+      setErrorMessage("");
+    }
+
+    else{
+      setBmi(null);
+      setBmiStatus("");
+      setErrorMessage("Enter valid numeric values for height and weight.");
+    }
+  };
+
+  const clearAll = () =>{
+    setBmi(null);
+    setBmiStatus("");
+    setHeight("");
+    setWeight("");
+    setErrorMessage("")
+  }
+
+  return (
+    <>
+    <div className="bmi-calculator">
+      <div className="box"></div>
+        <div className="data">
+          <h1>BMI Calculator</h1>
+          <p className='error'>{errorMessage}</p>
+          <div className="input-container">
+            <label htmlFor="height">Height (cm):</label>
+            <input type="text" id='height' value={height} onChange={(e)=>setHeight(e.target.value)}/>
+          </div>
+
+          <div className="input-container">
+            <label htmlFor="weight">Weight (kg):</label>
+            <input type="text" id='weight' value={weight} onChange={(e)=>setWeight(e.target.value)}/>
+          </div>
+
+          <button onClick={calculateBmi}>Calculate BMI</button>
+          <button onClick={clearAll}>Clear</button>
+
+          { bmi != null&& (<div className="result">
+             <p>Your BMI is: {bmi}</p>
+             <p>Status: {bmiStatus}</p>
+            </div>
+          )}
+
+        </div>
+    </div>
+    </>
+  )
+}
+
+export default App
